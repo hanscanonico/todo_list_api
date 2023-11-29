ccb6c988e6898b79bd8afcc962617d8608a6ee00
Hans Canonico <hanscanonico@air-de-hans.home>
2023-11-30 00:24:39 +0100

    add committee and update specs

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem "bootsnap", require: false
 
 gem 'rswag-api'
 gem 'rswag-ui'
+gem 'committee-rails'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- spec/requests/controllers/lists_controller_spec.rb
+++ spec/requests/controllers/lists_controller_spec.rb
@@ -1,38 +1,51 @@
 # frozen_string_literal: true
 
-require 'swagger_helper'
+require 'rails_helper'
 
-RSpec.describe ListsController, type: :controller do
-  describe '#create' do
+RSpec.describe ListsController, type: :request do
+  include Committee::Rails::Test::Methods
+
+  let!(:headers) { { 'Content-Type' => 'application/json' } }
+
+  describe 'POST /lists' do
     it 'creates a new list' do
       expect do
-        post :create, params: { list: { name: 'New List' } }
+        post lists_path, params: { name: 'New List' }.to_json,
+                         headers:
       end.to change(List, :count).by(1)
-      expect(response).to have_http_status(:created)
+
+      assert_request_schema_confirm
+      assert_response_schema_confirm(201)
+
       expect(JSON.parse(response.body)['name']).to eq('New List')
     end
   end
 
-  describe '#index' do
+  describe 'GET /lists' do
     let!(:list1) { List.create(name: 'List 1') }
     let!(:list2) { List.create(name: 'List 2') }
 
     it 'retrieves all lists' do
-      get :index
-      expect(response).to have_http_status(:ok)
+      get lists_path
+
+      assert_request_schema_confirm
+      assert_response_schema_confirm(200)
+
       expect(JSON.parse(response.body).size).to eq(2)
       expect(JSON.parse(response.body).map { |l| l['name'] }).to match_array(['List 1', 'List 2'])
     end
   end
 
-  describe '#destroy' do
+  describe 'DELETE /lists/{id}' do
     let!(:list) { List.create(name: 'List to delete') }
 
     it 'deletes a list' do
       expect do
-        delete :destroy, params: { id: list.id }
+        delete list_path(list)
       end.to change(List, :count).by(-1)
-      expect(response).to have_http_status(:ok)
+
+      assert_request_schema_confirm
+      assert_response_schema_confirm(200)
     end
 
     context 'when the list has tasks' do
@@ -40,34 +53,45 @@ RSpec.describe ListsController, type: :controller do
 
       it 'deletes the list and its tasks' do
         expect do
-          delete :destroy, params: { id: list.id }
+          delete list_path(list)
         end.to change(List, :count).by(-1).and change(Task, :count).by(-1)
-        expect(response).to have_http_status(:ok)
+
+        assert_request_schema_confirm
+        assert_response_schema_confirm(200)
       end
     end
 
     context 'when the list does not exist' do
+      let!(:list) { { id: 9999 } }
+
       it 'returns a not found error' do
-        delete :destroy, params: { id: 9999 }
-        expect(response).to have_http_status(:not_found)
+        delete list_path(list)
+
+        assert_request_schema_confirm
+        assert_response_schema_confirm(404)
+
         json_response = JSON.parse(response.body)
         expect(json_response['error']).to eq('List not found')
       end
     end
   end
 
-  describe '#update' do
+  describe 'PUT /lists/{id}' do
     let!(:list) { List.create(name: 'List to update') }
 
     it 'updates a list' do
-      put :update, params: { id: list.id, list: { name: 'Updated List' } }
+      put(list_path(list), params: { name: 'Updated List' }.to_json, headers:)
+
       expect(response).to have_http_status(:ok)
       expect(list.reload.name).to eq('Updated List')
     end
 
     context 'when the list does not exist' do
+      let!(:list) { { id: 9999 } }
+
       it 'returns a not found error' do
-        put :update, params: { id: 9999, list: { name: 'Updated List' } }
+        put(list_path(list), params: { name: 'Updated List' }.to_json, headers:)
+
         expect(response).to have_http_status(:not_found)
         json_response = JSON.parse(response.body)
         expect(json_response['error']).to eq('List not found')
